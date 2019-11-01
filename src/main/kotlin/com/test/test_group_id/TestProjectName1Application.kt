cfd0512b60b878ae467f14222c6e750f8f4817e1
basedown <basedown@basedowns-air.home>
2019-11-01 13:43:53 +0200

    Initial commit

---
+++ src/main/kotlin/com/test/test_group_id/TestProjectName1Application.kt
@@ -0,0 +1,11 @@
+package com.test.test_group_id
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestProjectName1Application
+
+fun main(args: Array<String>) {
+	runApplication<TestProjectName1Application>(*args)
+}
---
+++ src/test/kotlin/com/test/test_group_id/TestProjectName1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.test_group_id
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestProjectName1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
